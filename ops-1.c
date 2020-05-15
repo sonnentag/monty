8064f49cef035b84654b9a30416db74b8cbb2633
Tony Sims <zocle@zocle.net>
2020-05-14 21:31:03 -0500

    added push and pall

--- ops-1.c
+++ ops-1.c
@@ -1,4 +1,4 @@
-#include "monty.h"
+#include "monty.h" 
 
 /**
  * push - pushes to stack
@@ -7,8 +7,26 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	UNUSED(stack);
-	printf("line %i: push %i\n", line_number, valn);
+	stack_t *temp = NULL, *new = NULL;
+
+	if (!stack)
+		return;
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		free(new);
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = valn;
+	new->prev = NULL;
+
+	temp = *stack;
+	*stack = new;
+	new->next = temp;
+	if (temp)
+		temp->prev = *stack;
 }
 
 /**
@@ -18,8 +36,17 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	UNUSED(stack);
-	printf("line %d: pall\n", line_number);
+ 	stack_t *rev;
+ 
+	if (*stack)
+	{
+		rev = *stack;
+		while (rev)
+		{
+			printf("%d\n", rev->n);
+			rev = rev->next;
+		}
+	}
 }
 
 /**
