4f9bb63aa0eafb30e8b4a727c6ddb3511dbc64c1
Tony Sims <zocle@zocle.net>
2020-05-20 17:40:42 -0500

    changed valn assigment in pint

--- ops-1.c
+++ ops-1.c
@@ -74,7 +74,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (!(*stack))
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		valn = 0;
+		valn = -1;
 	}
 	printf("%d\n", (*stack)->n);
 }
